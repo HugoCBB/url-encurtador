1c807ba0d4edf46a9b740ca09dab8e16fb232a76
HugoCBB <hugocarlosb.071@gmail.com>
2026-03-22 18:05:53 -0300

    fix: adicionado cors

--- cmd/api/v1/routes.go
+++ cmd/api/v1/routes.go
@@ -9,6 +9,21 @@ import (
 	"github.com/hugocbb/url-encurtador/internal/controllers"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5174")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func healt(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"status":"ok"}`))
@@ -19,10 +34,11 @@ func HandlerRequest() {
 	rdb := config.NewClientRedis(ctx)
 	urlHandler := controllers.NewUrlController(rdb)
 
-	http.HandleFunc("/", healt)
-	http.HandleFunc("POST /", urlHandler.CreateUrl)
-	http.HandleFunc("GET /{code}", urlHandler.Redirect)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", healt)
+	mux.HandleFunc("POST /", urlHandler.CreateUrl)
+	mux.HandleFunc("GET /{code}", urlHandler.Redirect)
 
 	fmt.Println("Servidor iniciado em http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", corsMiddleware(mux))
 }
